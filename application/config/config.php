24850b6026f3fe77f8f0d31a39fb2102e3d110dd
John <ebarita00@gmail.com>
2020-02-07 14:05:32 +0800

    test server

--- application/config/config.php
+++ application/config/config.php
@@ -24,7 +24,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 */
 //$config['base_url'] = 'http://localhost/ptbcsi/';
-$config['base_url'] = 'http://localhost/ptbcsi/';
+//$config['base_url'] = 'http://localhost/ptbcsi/';
+$config['base_url'] = 'http://ubuntuserver.net/';
 
 /*
 |--------------------------------------------------------------------------
