30e5d05349ef7e7f4e35716390fcd40886903703
John Ebarita <ebarita00@gmail.com>
2020-03-11 11:56:26 +0800

    Fixed DTR

--- application/config/config.php
+++ application/config/config.php
@@ -22,9 +22,11 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | If you need to allow multiple domains, remember that this file is still
 | a PHP script and you can easily do that on your own.
 |
-*/
-//$config['base_url'] = 'http://localhost/ptbcsi/';
-$config['base_url'] = 'http://localhost:8081/ptbcsi/';
+//*/
+$config['base_url'] = 'http://localhost/ptbcsi/';
+//$config['base_url'] = 'http://localhost:8081/ptbcsi/';
+//$config['base_url'] = 'https://test2.estimit.net/~maricris/ptbcsi/';
+
 
 /*
 |--------------------------------------------------------------------------
