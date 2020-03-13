7fa3e7d1dbdfb73b483569f79230517d6db4ecc4
John Ebarita <ebarita00@gmail.com>
2020-03-13 17:01:36 +0800

    added cron

--- application/views/admin/cron.php
+++ application/views/admin/cron.php
@@ -5,33 +5,7 @@
  * Date: 09/03/2020
  * Time: 7:04 PM
  */
+include(getcwd() . '/application/views/admin/crontroller.php');
 
-$this->load->helper(array('url', 'html', 'form'));
-$this->load->library('pagination', 'session');
-$this->load->model(array('dtr_model', 'holiday_model', 'employee_model'));
-include(getcwd() . '/application/libraries/zklib/ZKLib.php');
-
-$zk = new ZKLib('43.224.189.146');
-if ($zk->connect()) {
-    $zk->disableDevice();
-    $attendance = $zk->getAttendance();
-    if (count($attendance) > 0) {
-        $attendances = array_reverse($attendances, true);
-        $users = $zk->getUser();
-        sleep(1);
-        foreach ($attendances as $item) {
-            $attendance = new stdClass();
-            $attendance->uid = $item['uid'];
-            $attendance->id = $item['id'];
-            $attendance->name = $users[$item['id']]['name'];
-            $attendance->state = ZK\Util::getAttState($item['state']);
-            $attendance->date = date("d-m-Y", strtotime($item['timestamp']));
-            $attendance->time = date("H:i:s", strtotime($attItem['timestamp']));
-            $attendance->type = ZK\Util::getAttType($item['type']);
-            $this->dtr_model->add_attendance($attendance);
-        }
-        $zk->clearAttendance();
-    }
-    $zk->enableDevice();
-    $zk->disconnect();
-}
+$con = new crontroller();
+$con->push();
---
+++ application/views/admin/crontroller.php
@@ -0,0 +1,46 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: User
+ * Date: 13/03/2020
+ * Time: 4:57 PM
+ */
+class crontroller extends CI_Controller
+{
+    public function __construct()
+    {
+        $this->load->helper(array('url', 'html', 'form'));
+        $this->load->library('pagination', 'session');
+        $this->load->model(array('dtr_model', 'holiday_model', 'employee_model'));
+        include(getcwd() . '/application/libraries/zklib/ZKLib.php');
+    }
+
+    public function push(){
+        $zk = new ZKLib('43.224.189.146');
+        if ($zk->connect()) {
+            $zk->disableDevice();
+            $attendances = $zk->getAttendance();
+            if (count($attendances) > 0) {
+                $attendances = array_reverse($attendances, true);
+                $users = $zk->getUser();
+                sleep(1);
+                foreach ($attendances as $item) {
+                    $attendance = new stdClass();
+                    $attendance->uid = $item['uid'];
+                    $attendance->id = $item['id'];
+                    $attendance->name = $users[$item['id']]['name'];
+                    $attendance->state = ZK\Util::getAttState($item['state']);
+                    $attendance->date = date("d-m-Y", strtotime($item['timestamp']));
+                    $attendance->time = date("H:i:s", strtotime($item['timestamp']));
+                    $attendance->type = ZK\Util::getAttType($item['type']);
+                    $this->dtr_model->add_attendance($attendance);
+                }
+                $zk->clearAttendance();
+            }
+            $zk->enableDevice();
+            $zk->disconnect();
+        }
+    }
+
+}
